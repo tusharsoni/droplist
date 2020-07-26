5e2f27e74c50f568f3fbb6e0e911bb7a93100041
Tushar Soni <tusharsoni1205@gmail.com>
2020-07-26 13:15:59 -0400

    Replace Table with List for templates page

--- src/pages/templates/index.js
+++ src/pages/templates/index.js
@@ -6,13 +6,12 @@ import useFetch from "use-http";
 import { StyledSpinnerNext as Spinner } from "baseui/spinner";
 import type { Template } from "../../lib/types/content";
 import { useStyletron } from "baseui";
-import { Display3, Label2, Paragraph3 } from "baseui/typography";
+import { Display3 } from "baseui/typography";
 import { Button, KIND, SIZE } from "baseui/button";
 import { Spacer40, Spacer8 } from "../../style-guide/spacer";
-import { Table } from "baseui/table-semantic";
-import { DateTime } from "luxon";
 import TemplateActionMenu from "./action-menu";
 import CreateTemplateButton from "./create-button";
+import { ListItem, ListItemLabel } from "baseui/list";
 
 const TemplatesPage = () => {
   const [css] = useStyletron();
@@ -49,48 +48,26 @@ const TemplatesPage = () => {
         <CreateTemplateButton />
       </div>
       <Spacer40 />
-      <Table
-        columns={["", "", ""]}
-        data={templates.map((t) => [
-          null,
-          <div className={css({ paddingTop: "20px" })}>
-            <Label2>{t.Name}</Label2>
-            <Paragraph3>
-              {t.Subject && (
-                <>
-                  {t.Subject}
-                  <br />
-                </>
-              )}
-              Last updated on{" "}
-              {DateTime.fromISO(t.UpdatedAt).toLocaleString(
-                DateTime.DATETIME_SHORT
-              )}
-            </Paragraph3>
-          </div>,
-          <div
-            className={css({
-              display: "flex",
-              float: "right",
-              paddingTop: "20px",
-            })}
+      <div>
+        {templates.map((template: Template) => (
+          <ListItem
+            key={template.UUID}
+            endEnhancer={() => (
+              <div className={css({ display: "flex" })}>
+                <TemplateActionMenu template={template} onUpdate={get} />
+                <Spacer8 />
+                <Button size={SIZE.compact} kind={KIND.secondary}>
+                  Create Campaign
+                </Button>
+              </div>
+            )}
           >
-            <TemplateActionMenu template={t} onUpdate={get} />
-            <Spacer8 />
-            <Button size={SIZE.compact} kind={KIND.secondary}>
-              Create Campaign
-            </Button>
-          </div>,
-        ])}
-        overrides={{
-          TableHead: {
-            style: { display: "none" },
-          },
-          TableBodyRow: {
-            style: { ":hover": { backgroundColor: "inherit" } },
-          },
-        }}
-      />
+            <ListItemLabel description={template.Subject}>
+              {template.Name}
+            </ListItemLabel>
+          </ListItem>
+        ))}
+      </div>
     </PageLayout>
   );
 };
